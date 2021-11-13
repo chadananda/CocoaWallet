8159dcaf9fac1cd4785172cc9e3cf350a096a216
Chad <chadananda@gmail.com>
2021-11-13 08:08:42 -0700

    tried replacing the binary with our new one

--- app/src/main/java/com/chadananda/cocoawallet/MiningService.java
+++ app/src/main/java/com/chadananda/cocoawallet/MiningService.java
@@ -155,7 +155,7 @@ public class MiningService extends Service {
                     "--hu;ge-pages-jit=TRUE --asm=auto --cpu-memory-pool=-1 --cpu-no-yield --print-time=8"+
                     "--retry-pause=2";
             String args = String.format(config_template, pool, wallet, max_bwt);
-            String binary = "./libpm.so";
+            String binary = "./lib-pocketminer.so";
             ProcessBuilder pb = new ProcessBuilder(binary, args);
 
             //in our directory, which is
